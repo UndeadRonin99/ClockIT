9efd934b2c482b09e6bdfecc1d2efc00fd7c62d3
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-04-22 17:25:06 +0200

    made some changes

--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/home_page.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/home_page.kt
@@ -1,11 +1,29 @@
 package com.varsitycollege.st10043352.opsc_clockit
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class home_page : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+    }
+
+    private fun navAddActivity() {
+    }
+
+    fun navAddCategory(view: View) {
+        startActivity(Intent(this, Add_Category::class.java))
+    }
+
+    fun navAddActivity(view: View) {
+        startActivity(Intent(this, AddActivity::class.java))
     }
 }
