22fd414ad8cdfddab30572ac1adfa18c1f63271f
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-04-24 23:08:29 +0200

    Test tyler's update

--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/home_page.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/home_page.kt
@@ -11,7 +11,6 @@ import android.widget.LinearLayout
 import android.widget.TextView
 
 class home_page : AppCompatActivity() {
-//test-tyler
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var txtActivity: TextView
 
