6083b70bda02e50943ff12d52a25fe411a37dcce
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-04-25 15:39:08 +0200

    reverting commits

--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/LogHours.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/LogHours.kt
@@ -10,26 +10,5 @@ class LogHours : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_hours)
-
-
-    }
-    fun formatSharedPref(activity: String?): CharSequence? {
-        var activityDetails = ""
-        activity?.let {
-            val values = activity.split(",")
-
-            val name = values[0]
-            val description = values[1]
-            val category = values[2]
-            val color = values[3]
-            val number1 = values[4]
-            val number2 = values[5]
-            val contentUri = values[6]
-
-            // Now you can use these variables as needed
-            // For example:
-            activityDetails= "${name}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${category}\n${number1}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t${number2}"
-        }
-        return activityDetails
     }
 }
