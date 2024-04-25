db67350b74161fddd50bd7f1cc11a970412eee73
TylerFriedman <st10161340@vcconnect.edu.za>
2024-04-25 16:48:54 +0200

    log hours activity name and category display

--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/LogHours.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/LogHours.kt
@@ -1,14 +1,26 @@
 package com.varsitycollege.st10043352.opsc_clockit
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LogHours : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_hours)
+
+        val activityData = intent.getStringExtra("activityData") ?: ""
+        val activityTextView = findViewById<TextView>(R.id.textView_activity_name)
+        val categoryTextView = findViewById<TextView>(R.id.textView_activity_category)
+
+        // Split the activity details
+        val details = activityData.split(",")
+        if (details.size >= 3) { // Ensure we have at least three elements (name, description, category)
+            val activityName = details[0] // Activity Name
+            val categoryName = details[2] // Category Name
+
+            activityTextView.text = activityName
+            categoryTextView.text = categoryName
+        }
     }
 }
