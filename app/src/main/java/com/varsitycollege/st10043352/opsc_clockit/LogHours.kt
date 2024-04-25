4af137cc1107544b38a6140d7cfb6f07b8afda33
TylerFriedman <st10161340@vcconnect.edu.za>
2024-04-25 17:21:01 +0200

    log hours activity name and category display and the back button feature now works

--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/LogHours.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/LogHours.kt
@@ -1,6 +1,9 @@
 package com.varsitycollege.st10043352.opsc_clockit
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -21,6 +24,15 @@ class LogHours : AppCompatActivity() {
 
             activityTextView.text = activityName
             categoryTextView.text = categoryName
+        }
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            // Navigate back to the home page
+            val intent = Intent(this, home_page::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Clear the back stack
+            startActivity(intent)
+            finish() // Finish current activity
         }
     }
 }
