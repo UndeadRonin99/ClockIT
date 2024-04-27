9662add02dc806e5e8809cffb5d86a572495c8e2
TylerFriedman <st10161340@vcconnect.edu.za>
2024-04-27 18:40:56 +0200

    insights page calendar done with start and end date working

--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/stats.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/stats.kt
@@ -3,16 +3,56 @@ package com.varsitycollege.st10043352.opsc_clockit
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class stats : AppCompatActivity() {
+    private lateinit var startDate: TextView
+    private lateinit var endDate: TextView
+    private lateinit var calendarView: CalendarView
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+    private var selectedStartDate: Date? = null
+    private var selectedEndDate: Date? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
+        startDate = findViewById(R.id.StartDate)
+        endDate = findViewById(R.id.EndDate)
+        calendarView = findViewById(R.id.calendarView)
+
+        // Set listener for calendar view
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = getDateFromDatePicker(year, month, dayOfMonth)
+            if (selectedStartDate == null || selectedEndDate != null) {
+                // Set the selected date as start date if no start date is selected or if end date is already selected
+                selectedStartDate = selectedDate
+                selectedEndDate = null
+                startDate.text = "Start Date: ${dateFormat.format(selectedDate)}"
+                endDate.text = ""
+            } else if (selectedStartDate != null && selectedEndDate == null) {
+                // Set the selected date as end date if start date is already selected but end date is not
+                selectedEndDate = selectedDate
+                endDate.text = "End Date: ${dateFormat.format(selectedDate)}"
+            }
+        }
     }
+
+    // Function to get Date from DatePicker
+    private fun getDateFromDatePicker(year: Int, month: Int, day: Int): Date {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        return calendar.time
+    }
+
 
     fun navInsights(view: View) {
         startActivity(Intent(this, Insights::class.java))
