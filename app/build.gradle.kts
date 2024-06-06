7fd9a72ce8af5074deb1000777ef042554d9bebd
TylerFriedman <st10161340@vcconnect.edu.za>
2024-06-06 12:21:11 +0200

    sdk added for graph and a rough graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.0")
@@ -52,5 +53,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 
 }
--- app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/PeriodLogged.kt
+++ app/src/main/java/com/varsitycollege/st10043352/opsc_clockit/PeriodLogged.kt
@@ -301,4 +301,3 @@ class PeriodLogged : AppCompatActivity() {
         finish()
     }
 }
-
